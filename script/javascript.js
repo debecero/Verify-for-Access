89cd2a3c1513cf06b47edcd2e2a41488c9e9d233
debecero <dennis.berghall@gmail.com>
2021-01-15 11:35:05 +0100

    Draft

---
+++ script/javascript.js
@@ -0,0 +1,25 @@
+window.onscroll = function() {mySticky("myHeader")};
+
+var header = document.getElementById("myHeader");
+var mySticky = header.offsetTop;
+
+function mySticky() {
+  if ((window.pageYOffset) >= 50); {
+    header.classList.add ("sticky");
+  } else {
+    header.classList.remove ("sticky");
+  }
+
+  function newFunction() {
+    ;
+  }
+}
+
+window.addEventListener('scroll', checkScrollPosition);
+function checkScrollPosition(){
+  if ((window.pageYOffset) >= 240) {
+  var hero = document.getElementById('myHeader')
+  sticky.style.position = "fixed"
+  sticky.style.top = "-140px"
+}
+}
