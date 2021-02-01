e0913b993ebc7cb85dcbc33e3db5a4a222a94d91
debecero <dennis.berghall@gmail.com>
2021-02-01 22:12:43 +0100

    Beta

--- script/function.js
+++ script/function.js
@@ -2,24 +2,23 @@
  
   'use strict';
  
-  document.querySelector('.material-design-hamburger__icon').addEventListener(
+  document.querySelector('.material-design-hamburger_icon').addEventListener(
     'click',
-    function() {      
+    function openClick() {  
+      console.log ("öppna")     
       var child;
       
-      document.body.classList.toggle('background--blur');
-      this.parentNode.nextElementSibling.classList.toggle('menu--on');
+      document.body.classList.toggle('background-blur');
+      this.parentNode.nextElementSibling.classList.toggle('menu-on');
  
       child = this.childNodes[1].classList;
  
-      if (child.contains('material-design-hamburger__icon--to-arrow')) {
-        child.remove('material-design-hamburger__icon--to-arrow');
-        child.add('material-design-hamburger__icon--from-arrow');
+      if (child.contains('material-design-hamburger_icon-to-arrow')) {
+        child.remove('material-design-hamburger_icon-to-arrow');
+        child.add('material-design-hamburger_icon-from-arrow');
       } else {
-        child.remove('material-design-hamburger__icon--from-arrow');
-        child.add('material-design-hamburger__icon--to-arrow');
+        child.remove('material-design-hamburger_icon-from-arrow');
+        child.add('material-design-hamburger_icon-to-arrow');
       }
- 
     });
- 
 })();
